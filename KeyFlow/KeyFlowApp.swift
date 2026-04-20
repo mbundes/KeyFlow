f293bf67d95fc7264b8022a1418cfec0dde80947
MK <kozlov@icloud.com>
2026-04-20 16:59:43 +0300

    Initial Commit

---
+++ KeyFlow/KeyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyFlowApp.swift
+//  KeyFlow
+//
+//  Created by MK on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct KeyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
